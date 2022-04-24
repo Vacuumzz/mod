1c77fee6d9633d92520ba61cb1742343b4614e8c
Vacuumzz <101456513+Vacuumzz@users.noreply.github.com>
2022-04-24 17:18:19 +0300

    Create .s

---
+++ .s
@@ -0,0 +1,8 @@
+from .. import loader, utils
+
+Class CalculatorMod(loader.Module):
+Strings={"name":"Тест"}
+async def xcmd(self.message): 
+await message.edit("<b>Ок</b>")
+args = all.message
+text = args.write(".e "all.message" - "text")
