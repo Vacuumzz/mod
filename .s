87c030dc73fd45c427227f78c2b9f7c4eb0ae1b7
Vacuumzz <101456513+Vacuumzz@users.noreply.github.com>
2022-04-24 17:21:24 +0300

    Delete .s

--- .s
+++
@@ -1,9 +0,0 @@
-from .. import loader, utils
-
-Class CalculatorMod(loader.Module):
-Strings={"name":"Тест"}
-async def xcmd(self.message): 
-await message.edit("<b>Ок</b>")
-args = all.message
-await.message from.id
-text = args.write(".e "all.message" - "from.id")
